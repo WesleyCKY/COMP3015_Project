ead39ac97a60574b96024af90a00213ce2a69ad0
e9220537 <e9220537@csdlabad.local>
2019-11-05 15:47:09 +0800

    Added lines in SimpleClient.java

--- src/SimpleClient.java
+++ src/SimpleClient.java
@@ -8,13 +8,18 @@ public class SimpleClient {
 	public SimpleClient (String name) throws IOException {
 	String msg = "Name: "+name;
 	String content; // read server content
+	InetAddress serverAddr= InetAddress.getByName("158.182.8.162");
 	
-	DatagramSocket receivedSocket = new DatagramSocket(4321); // socket used for receving
+	DatagramSocket receivedSocket = new DatagramSocket(1234); // socket used for receving
+	DatagramSocket sentSocket = new DatagramSocket(4321);
 	DatagramPacket receivedPacket = new DatagramPacket(new byte[1024], 1024);
+	DatagramPacket sentPacket = new DatagramPacket(msg.getBytes(), msg.length(), serverAddr, 4321);
+	
+	sentSocket.send(sentPacket);
+	System.out.println("Sent!!!");
 	
 	while(true) {
 		System.out.println("Listening...");
-		
 		receivedSocket.receive(receivedPacket); // Receive server's response
 		System.out.println("Received");
 	}

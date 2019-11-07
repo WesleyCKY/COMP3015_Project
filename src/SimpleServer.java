d5b23fd554e5b285fbe960bdcfd43c7d9c7eda96
e7225736 <e7225736@csdlabad.local>
2019-11-07 18:32:12 +0800

    create and send default sketch

--- src/SimpleServer.java
+++ src/SimpleServer.java
@@ -8,7 +8,21 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SimpleServer {
+
+	private Socket clientSocket;
+	byte[] buffer = new byte[1024];
+	private DataInputStream in;
+	private DataOutputStream out;
+	int[][] data = new int[50][50];
+
 	public SimpleServer() throws IOException {
+		// default sketch data
+		for (int i = 0; i <= 49; i++) {
+			for (int j = 0; j <= 49; j++) {
+				data[i][j] = -543230;
+			}
+		}
+
 		int svrport = 1234;
 		DatagramSocket socket = new DatagramSocket(svrport);
 		DatagramPacket receivedPacket = new DatagramPacket(new byte[1024], 1024);
@@ -41,30 +55,69 @@ public class SimpleServer {
 		socket.close();
 		// establish a TCP connection
 		ServerSocket srvSocket = new ServerSocket(svrport);
+		// accept connection
+		clientSocket = srvSocket.accept();
+		// establish a TCP connection
+		in = new DataInputStream(clientSocket.getInputStream());
+		out = new DataOutputStream(clientSocket.getOutputStream());
 
 		System.out.printf("TCP server is listening at port %d...", svrport);
-		Socket clientSocket = srvSocket.accept();
-		System.out.println("Accepted!");
-		server(clientSocket);
 
+		System.out.println("Accepted!");
+
+		server(clientSocket);
+		send();
+		receive();
+
+	}
+
+	public void send() throws IOException {
+		int defaultFileSize = 0; 
+		// get the data cols and rows of default sketch
+		out.writeInt(50);
+		out.writeInt(50);
+
+//		send out the default sketch size 
+		out.writeInt(defaultFileSize); 
+		System.out.println("The file size is sent!!");
+//		send out the default sketch data
+		for (int[] i : data) {
+			for (int p : i) {
+				out.writeInt(p);
+			}
+		}
+		System.out.println("The default sketch is sent!");
+	}
+
+	public void receive() throws IOException { // send color pixels
+		// int[] pixelData = data;
+		// for (int[] i : pixelData) {
+		// int pix = data;
+		while (true) {
+			int pix = in.readInt(); // receive a pix
+			System.out.println(pix);
+			int col = in.readInt();
+			System.out.println(col);
+			int row = in.readInt();
+			System.out.println(row);
+			data[row][col] = pix;
+		}
 	}
 
 	private void server(Socket clientSocket) throws IOException {
 		// buffer
-		byte[] buffer = new byte[1024];
+
 		// display the client socket and port number
 		System.out.printf("Established a connection to host %s:%d\n\n", clientSocket.getInetAddress(),
 				clientSocket.getPort());
 		// set up data input and output stream
-		DataInputStream in = new DataInputStream(clientSocket.getInputStream());
-		DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
 
 		System.out.println("Established the in/output stream");
 
 		// int len = in.readInt();
 		// in.read(buffer, 0, len);
 		// System.out.println(buffer.toString());
-		String str = "The message sent by TCP is:";
+		// String str = "The message sent by TCP is:";
 
 		// System.out.println("message is prepared!");
 		// send out the message by TCP
@@ -73,20 +126,20 @@ public class SimpleServer {
 		// out.write(str.getBytes(), 0, str.length());
 		// System.out.println("message conrtent is sent!");
 		int data[][] = { { 0, 1, 2 }, { 3, 4, 5 } };
-		int fileSize=0;
-		for (int i[]: data) {
-			fileSize+=i.length;
+		int fileSize = 0;
+		for (int i[] : data) {
+			fileSize += i.length;
 		}
 		out.writeInt(fileSize);
 		System.out.println("The file size is sent!!");
-		
+
 		for (int[] i : data) {
 			for (int p : i) {
 				out.writeInt(p);
 				System.out.println(p);
 			}
 		}
-		clientSocket.close();
+		// clientSocket.close();
 	}
 
 }
